7efe4c397533caf21fd25cfdc806c62363a1b457
oliselvan <oselvan37@gmail.com>
2026-03-23 11:00:41 +0530

    Fix catch-all route to avoid path-to-regexp parsing error

--- server.ts
+++ server.ts
@@ -416,7 +416,7 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     app.use(express.static(path.join(__dirname, "dist")));
-    app.get("*", (_req, res) => {
+    app.get("/*", (_req, res) => {
       res.sendFile(path.join(__dirname, "dist", "index.html"));
     });
   }
